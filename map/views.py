e25bed534b4e023988c197baf11859bf6d0d4dae
Robert Bulcock <robert.bulcock@icloud.com>
2025-09-05 16:33:28 +0100

    map view working

---
+++ map/views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render
+
+# Create your views here.
+def map_view(request):
+    return render(request, "map/planner_map.html")
--- planner/urls.py
+++ planner/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from map.views import map_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', map_view, name='map_view'),  # Set the root URL to show the map
 ]
